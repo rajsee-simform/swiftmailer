c365fe66ea2a9881bfb51cd30792dc616832916e
rajsee-simform <122426694+rajsee-simform@users.noreply.github.com>
2023-05-22 14:48:57 +0530

    Update swift_required.php
    To avoid the error "Can not declare Swift class more than once"

--- lib/swift_required.php
+++ lib/swift_required.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-require __DIR__.'/classes/Swift.php';
+require_once __DIR__.'/classes/Swift.php';
 
 Swift::registerAutoload(function () {
     // Load in dependency maps
